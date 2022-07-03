a5e5dacd920a2cd3988a9da63446a86ddd91fa19
Kirill-Kondratev <kondratevk.1989@mail.ru>
2022-07-03 18:33:20 +0700

    add all lessons

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import App from "./App";
 import store from "./store";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faCircleCheck,
+  faCircleExclamation,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCircleCheck, faCircleExclamation, faXmark);
+
 store.dispatch("products/load").then(() => {
-  createApp(App).use(store).use(router).mount("#app");
+  createApp(App)
+    .use(store)
+    .use(router)
+    .component("fa-icon", FontAwesomeIcon)
+    .mount("#app");
 });
 
 import "bootstrap/dist/css/bootstrap.min.css";
